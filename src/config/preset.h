e6de971a70925a6498cb6798392211b8a3978264
max-amb <max_a@e.email>
2026-05-10 13:53:41 +0100

    Added buttons and moved back to monitor mapping

--- src/config/preset.h
+++ src/config/preset.h
@@ -23,12 +23,12 @@ static const TabletButton tabletbuttons[] = {
 static const TabletPadButton tabletpadbuttons[] = {
 	/* button  state                  function          argument */
 /*  { 0,       WLR_BUTTON_PRESSED,    spawn,            {.v = termcmd} }, */
-/*  { 1,       WLR_BUTTON_PRESSED,    killclient,       {0} },*/
+  { 1,       WLR_BUTTON_PRESSED,    killclient,       {0} },
 /*  { 2,       WLR_BUTTON_PRESSED,    togglefloating,   {0} },*/
 /*  { 3,       WLR_BUTTON_PRESSED,    zoom,             {0} },*/
 	{ 0,            0,                     NULL,       {0} }
 };
 
 /* Pen active area: value 0.0 => 1.0         { x,    y,    width, height } */
-static const int tabletmaptosurface        = 1;  /* map tablet input to surface(1) or monitor(0) */
+static const int tabletmaptosurface        = 0;  /* map tablet input to surface(1) or monitor(0) */
 static const double tablet_area[4] 		   = { 0.0,  0.0,  1.0,   1.0 };
